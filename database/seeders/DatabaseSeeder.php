5965463228e1e38fed3bcf46069f08558ee41faa
aku ibnnu <ibnudev@medeva.tech>
2025-03-03 07:23:35 +0700

    feat: Add MasterSeeder to initialize various data types in the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,6 @@
 <?php
 
 namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +13,7 @@ class DatabaseSeeder extends Seeder
             VirusSeeder::class,
             SerotypeSeeder::class,
             MorphotypeSeeder::class,
+            MasterSeeder::class
         ]);
     }
 }
